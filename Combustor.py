03ca79efc1240c03aaf544ad988f6563cefbdeb4
louvill <villal@purdue.edu>
2019-12-14 14:15:46 -0500

    array sort

--- Combustor.py
+++ Combustor.py
@@ -44,7 +44,7 @@ class Combustor:
         self.cp = self.cp*np.ones(round(self.l/self.stepSize)+1)
         self.R = self.R*np.ones(round(self.l/self.stepSize)+1)
         
-        for i in range(0, round(self.l/self.stepSize)):
+        for i in range(round(self.l/self.stepSize)):
             cpair = (27.453+6.1838*(self.T[1][i]/1000)+.89932*(self.T[1][i]/1000)**2)/28*1000
             cpH2 = (26.896+4.3501*(self.T[1][i]/1000)-.32674*(self.T[1][i]/1000)**2)/2*1000
             gair = cpair/(cpair-self.R[0])
@@ -72,16 +72,22 @@ class Combustor:
             
             self.P[1][i+1] = self.mf[i+1]/(self.A[i+1]*self.M[i+1])*np.sqrt(self.R[i+1]*self.T[1][i+1]/self.g[i+1])
             self.P[0][i+1] = self.P[1][i+1]*(self.T[0][i+1]/self.T[1][i+1])**(self.g[i+1]/(self.g[i+1]-1))
+            #if self.P[0][i+1] > 1e10:
+            #    print()
+            #    print(self.P[1][i+1])
+            #    print(self.T[0][i+1])
+            #    print(self.T[1][i+1])
+            #    print(self.g[i+1])
+            #    print()
+            if self.P[0][i+1] < self.targetExitPressure:
+                break
             
             self.v[i+1] = self.M[i+1]*np.sqrt(self.g[i+1]*self.R[i+1]*self.T[1][i+1])
         
         if self.P[1][len(self.P)-1] < self.targetExitPressure or min(self.M) < 1.05:
-            for j in range(0, len(self.a)-1):
-                if j == 0 or j == len(self.a)-1:
-                    self.a[j] = 0
-                else:
-                    self.a[j] = random.random()
-            for j in range(0, len(self.slope)-1):
+            for j in range(1, len(self.a)-1):
+                self.a[j] = random.random()
+            for j in range(len(self.slope)):
                 self.slope[j] = np.pi/2*random.random()
             self.calcPerformance()
     
@@ -92,14 +98,14 @@ class Combustor:
         if x == self.l:
             a = self.a(len(self.a))
         else:
-            for i in range(0, len(self.a)-1):
+            for i in range(len(self.a)):
                 if i*adx <= x and x < (i+1)*adx: 
                     m = (self.a[i+1]-self.a[i])/adx
                     a = m*(x-adx*i)+self.a[i]
         return a
 
     def mutateFuelCoeff(self, mutRate):
-        for i in range(1, len(self.a)-2):
+        for i in range(1, len(self.a)-1):
             if round(mutRate*random.random()) == 0:
                 self.a[i] = random.random()*max(self.a)
 
--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -2,6 +2,7 @@ import os
 import math
 import numpy as np
 import random
+from copy import copy
 from Combustor import *
 
 os.system("cls")
@@ -37,7 +38,7 @@ cp0 = 1308
 numParents = 20
 numChildren = 20
 generationSize = numParents+numChildren
-numGenerations = round(1e2)
+numGenerations = round(1e0)
 graphUpdateFrequency = 10
 numFuelCoeff = 10
 baseMutationRate = 5
@@ -45,19 +46,34 @@ mutationRate = baseMutationRate
 
 carray = np.empty(generationSize, dtype=Combustor)
 
-for i in range(0, generationSize-1):
+print('Initializing combustor array...')
+for i in range(generationSize):
     a = np.zeros(numFuelCoeff)
-    for j in range(0, numFuelCoeff-1):
-        if j == 0 or j == numFuelCoeff-1:
-            a[j] = 0
-        else:
-            a[j] = random.random()
-
+    for j in range(1,numFuelCoeff-1):
+        a[j] = random.random()
     slope = np.zeros(len(slope))
     for j in range(0, len(slope)-1):
         slope[j] = np.pi/2*random.random()
     carray[i] = Combustor(Tt0, Pt2, 100, M2, dx, lt, slope, a, P0, cp0)
     carray[i].calcPerformance()
-    print(carray[i].getExitV())
+    #print(carray[i].getExitV())
     
 genHistory = np.zeros(numGenerations)
+
+for i in range(numGenerations):
+    print('Current Generation: ' + str(i+1))
+
+    sortComplete = False
+    while sortComplete == False:
+        sortComplete = True
+        for j in range(len(carray)-1):
+            if carray[j].getExitV() < carray[j+1].getExitV():
+                ctemp = copy(carray[j])
+                carray[j] = copy(carray[j+1])
+                carray[j+1] = copy(ctemp)
+                sortComplete = False
+
+    for j in range(5):
+        print('Exit velocity for element ' + str(j+1) + ': ' + str(carray[j].getExitV()))
+
+    genHistory[i] = carray[0].getExitV()
